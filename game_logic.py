53b71662d2799ea1ce7ff8fa76b47b151fb73e62
BernardasJuzumas <bernardas@juzumas.com>
2024-07-31 11:38:14 +0300

    some rudimentary logging in game logic to know when reroll happened

--- game_logic.py
+++ game_logic.py
@@ -1,7 +1,10 @@
 import random
 from config import SYMBOLS, SYMBOL_REWARDS
+import logging
 
-
+# Set up logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
+logger = logging.getLogger(__name__)
 def roll(credits):
     if credits < 40:
         return roll_slots()
@@ -12,6 +15,7 @@ def roll(credits):
     
 
 def roll_slots():
+    logger.info("Rolled {symbols}")
     return [random.choice(SYMBOLS) for _ in range(3)]
 
 
@@ -19,6 +23,7 @@ def cheat_roll(reroll_chance):
     symbols = roll_slots()
     #if all symbols are the same check of reroll and reroll
     if len(set(symbols)) == 1 and random.random() < reroll_chance:
+        logger.info("Rerolling")
         return cheat_roll(reroll_chance)
     return symbols
 
